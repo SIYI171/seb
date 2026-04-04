fbaa6f903096b9a1407b3390b142fb3420f90d44
SIYI171 <244800910@qq.com>
2026-04-05 00:22:43 +0800

    refactor(entity): 为shareToken字段添加更新策略注解
    添加@TableField注解并设置updateStrategy为FieldStrategy.IGNORED，允许该字段在更新时被忽略

--- backend/src/main/java/com/seb/entity/Website.java
+++ backend/src/main/java/com/seb/entity/Website.java
@@ -1,6 +1,8 @@
 package com.seb.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldStrategy;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -14,6 +16,7 @@ public class Website {
     private String name;
     private String domain;
     private String trackingId;
+    @TableField(updateStrategy = FieldStrategy.IGNORED)
     private String shareToken;
     private LocalDateTime createdAt;
 }
